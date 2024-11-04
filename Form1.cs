b407fcbefeaf9c342d4a73e18374e97046e394de
opilane <opilane@HP-CZC2349HV1>
2024-11-04 15:44:03 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangle_vorm
+{
+    public partial class Form1 : Form
+    {
+        Button startBtn;
+        ListView listView;
+        TextBox textBox1;
+        TextBox textBox2;
+        TextBox textBox3;
+        TextBox textBox4;
+
+        public Form1()
+        {
+            Width = 800;
+            Height = 400;
+            Text = "Töö kolmnurgaga";
+
+            startBtn = new Button();
+            startBtn.Text = "Käivita";
+            startBtn.Size = new Size(100, 60);
+            startBtn.Location = new Point(600,40);
+            startBtn.BackColor = Color.CornflowerBlue;
+            startBtn.Font = new Font("Arial",15,FontStyle.Bold);
+            startBtn.Cursor = Cursors.Hand;
+            startBtn.FlatAppearance.BorderColor = Color.Aqua;
+            startBtn.FlatAppearance.BorderSize = 4;
+            startBtn.FlatStyle = FlatStyle.Flat;
+            Controls.Add(startBtn);
+
+            listView = new ListView();
+            listView.Size = new Size(350,200);
+            listView.Location = new Point(50,30);
+            Controls.Add(listView);
+            
+            textBox1 = new TextBox();
+            textBox1.Size = new Size(100, 40);
+            textBox1.Location = new Point(50,250);
+            Controls.Add(textBox1);
+
+            textBox2 = new TextBox();
+            textBox2.Size = new Size(100, 40);
+            textBox2.Location = new Point(50, 300);
+            Controls.Add(textBox2);
+
+            textBox3 = new TextBox();
+            textBox3.Size = new Size(100, 40);
+            textBox3.Location = new Point(170, 250);
+            Controls.Add(textBox3);
+
+            textBox4 = new TextBox();
+            textBox4.Size = new Size(100, 40);
+            textBox4.Location = new Point(170, 300);
+            Controls.Add(textBox4);
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_vorm
+{
+    enum TriangleSide
+    {
+        A,
+        B,
+        C
+    }
+
+    internal class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+        private double h;
+
+        public Triangle(TriangleSide A, TriangleSide B, TriangleSide C)
+        {
+            a = (double)A;
+            b = (double)B;
+            c = (double)C;
+        }
+
+        public Triangle(TriangleSide A, TriangleSide B, TriangleSide C, double height)
+        {
+            a = (double)A;
+            b = (double)B;
+            c = (double)C;
+            h = height;
+        }
+
+        public Triangle(TriangleSide side, double height)
+        {
+            switch (side)
+            {
+                case TriangleSide.A:
+                    a = height;
+                    b = 0;
+                    c = 0;
+                    break;
+                case TriangleSide.B:
+                    b = height;
+                    a = 0;
+                    c = 0;
+                    break;
+                case TriangleSide.C:
+                    c = height;
+                    a = 0;
+                    b = 0;
+                    break;
+            }
+            h = height;
+        }
+
+
+        public string outputA()
+        {
+            return Convert.ToString(a);
+        }
+
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+
+        public string outputH()
+        {
+            return Convert.ToString(h);
+        }
+
+        public double HeightFromA()
+        {
+            double s = Surface();
+            return (2 * s) / a; 
+        }
+
+        public double HeightFromB()
+        {
+            double s = Surface();
+            return (2 * s) / b; 
+        }
+
+        public double HeightFromC()
+        {
+            double s = Surface();
+            return (2 * s) / c;
+        }
+
+        public double Perimeter()
+        {
+            double p = 0;
+            p = a + b + c;
+            return p;
+        }
+
+        public double Surface()
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+
+        public double GetSetA
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public double GetSetH
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        public bool ExistTrtiangle
+        {
+            get 
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))
+                    return false;
+                else return true;
+            }
+        }
+    }
+}
